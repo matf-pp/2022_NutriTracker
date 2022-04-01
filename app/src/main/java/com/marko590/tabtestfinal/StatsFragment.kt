ca4d9d7d17f8f93f67f9398d111a7807d6b64ba6
Marko590 <markobojkovicmb@gmail.com>
2022-04-01 22:48:49 +0200

    Added a sample pie chart to test

--- app/src/main/java/com/marko590/tabtestfinal/StatsFragment.kt
+++ app/src/main/java/com/marko590/tabtestfinal/StatsFragment.kt
@@ -1,11 +1,19 @@
 package com.marko590.tabtestfinal
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.marko590.tabtestfinal.databinding.StatsFragmentBinding
 
 
@@ -15,6 +23,37 @@ class StatsFragment : Fragment() {
         val binding = DataBindingUtil.inflate<StatsFragmentBinding>(inflater,
             R.layout.stats_fragment,container,false)
         binding.textView.text="Statistics"
+
+        val pieChart : PieChart = binding.pieChart
+        val entries: ArrayList<PieEntry> = ArrayList()
+        entries.add(PieEntry(0.2f, "Food & Dining"))
+        entries.add(PieEntry(0.15f, "Medical"))
+        entries.add(PieEntry(0.10f, "Entertainment"))
+        entries.add(PieEntry(0.25f, "Electricity and Gas"))
+        entries.add(PieEntry(0.3f, "Housing"))
+
+        val colors: ArrayList<Int> = ArrayList()
+        for (color in ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color)
+        }
+
+        for (color in ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color)
+        }
+
+        val dataSet = PieDataSet(entries, "Expense Category")
+        dataSet.colors = colors
+
+        val data = PieData(dataSet)
+        data.setDrawValues(true)
+        data.setValueFormatter(PercentFormatter(pieChart))
+        data.setValueTextSize(12f)
+        data.setValueTextColor(Color.BLACK)
+
+        pieChart.setData(data)
+        pieChart.invalidate()
+
+        pieChart.animateY(1400, Easing.EaseOutQuart)
         return binding.root
     }
 
