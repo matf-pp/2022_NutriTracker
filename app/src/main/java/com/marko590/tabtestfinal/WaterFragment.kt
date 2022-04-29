8c792b149de4407688bbab5486a83166070c9013
Marko590 <markobojkovicmb@gmail.com>
2022-04-29 16:24:01 +0200

    Reverted changes

--- app/src/main/java/com/marko590/tabtestfinal/WaterFragment.kt
+++ app/src/main/java/com/marko590/tabtestfinal/WaterFragment.kt
@@ -19,52 +19,6 @@ class WaterFragment : Fragment() {
         val binding = DataBindingUtil.inflate<WaterFragmentBinding>(inflater,
             R.layout.water_fragment,container,false)
 
-        val progressBar = binding.pbWaterIntake
-        val sharedPrefIntake = requireActivity().getSharedPreferences("mySharedPref", Activity.MODE_PRIVATE)
-        val glassSize = resources.getStringArray(R.array.glassSize)
-        val spinner = binding.sGlasses
-        if(spinner != null) {
-            val adapter = ArrayAdapter(
-                requireContext(),
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                glassSize
-            )
-            spinner.adapter = adapter
-        }
-
-        fun calculatePercentage(curr: Int, target: Int) : Int {
-            return (curr / (target / 100.0)).roundToInt()
-        }
-        val editorProgress = sharedPrefIntake.edit()
-
-        var progress = sharedPrefIntake.getInt("waterProgress", 0)
-        var waterToday = sharedPrefIntake.getInt("waterIntake", 0)
-        binding.tvWaterIntake.text = waterToday.toString()+"ml/2000ml"
-        progressBar.progress=progress
-        binding.tvWaterProgress.text = "("+progress.toString()+"%)"
-        binding.btnPlus.setOnClickListener {
-            if(progress < 100) {
-                var selectedSize = spinner.selectedItem.toString().take(3).toInt()
-
-                waterToday = waterToday + selectedSize
-                progress = (100.0 * waterToday / 2000).toInt()
-                if(progress > 100){
-                    progress = 100
-
-                }
-                binding.tvWaterIntake.text = waterToday.toString()+"ml/2000ml"
-                binding.tvWaterProgress.text = "("+progress.toString()+"%)"
-                sharedPrefIntake.edit().putInt("waterProgress", progress).apply()
-                sharedPrefIntake.edit().putInt("waterIntake", waterToday).apply()
-
-                progressBar.progress = progress
-
-
-
-            }
-
-
-        }
 
 
         return binding.root
