6e1f1cf1f5d5f165bcd4854fc5899f0898123212
TamaraS1604 <mi19085@alas.matf.bg.ac.rs>
2022-05-04 18:35:11 +0200

    Finished Water Fragment

--- app/src/main/java/com/marko590/tabtestfinal/WaterFragment.kt
+++ app/src/main/java/com/marko590/tabtestfinal/WaterFragment.kt
@@ -1,7 +1,7 @@
 package com.marko590.tabtestfinal
 
 import android.app.Activity
-import android.content.Context
+import android.hardware.SensorEventListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +10,72 @@ import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.marko590.tabtestfinal.databinding.WaterFragmentBinding
-import kotlin.math.round
 import kotlin.math.roundToInt
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorManager
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import kotlinx.android.synthetic.main.water_fragment.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-class WaterFragment : Fragment() {
+
+class WaterFragment : Fragment(), SensorEventListener {
+
+    var running = false
+    var sensorManager:SensorManager? = null
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepsSensor == null) {
+            Toast.makeText(requireContext(), "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+
+        val sharedPref = requireContext().getSharedPreferences("mySharedPref", Context.MODE_PRIVATE)
+        val stepsGoal = sharedPref.getInt("stepsGoal", 0)
+        if (running) {
+            if(event.values[0].toInt() > stepsGoal){
+                event.values[0] = stepsGoal.toFloat()
+            }
+            tvStepsProgress.text = "" + event.values[0].toInt() + "/$stepsGoal"
+        }
+
+        val progress = ((event.values[0].toInt() * 100) / stepsGoal)
+        pbStepsTracker.progress = progress
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        super.onCreateView(inflater, container, savedInstanceState)
+
         val binding = DataBindingUtil.inflate<WaterFragmentBinding>(inflater,
             R.layout.water_fragment,container,false)
 
@@ -33,9 +93,6 @@ class WaterFragment : Fragment() {
             spinner.adapter = adapter
         }
 
-        fun calculatePercentage(curr: Int, target: Int) : Int {
-            return (curr / (target / 100.0)).roundToInt()
-        }
         val editorProgress = sharedPrefIntake.edit()
 
         var progress = sharedPrefIntake.getInt("waterProgress", 0)
@@ -45,7 +102,7 @@ class WaterFragment : Fragment() {
         binding.tvWaterProgress.text = "("+progress.toString()+"%)"
         binding.btnPlus.setOnClickListener {
             if(progress < 100) {
-                var selectedSize = spinner.selectedItem.toString().take(3).toInt()
+                val selectedSize = spinner.selectedItem.toString().take(3).toInt()
 
                 waterToday = waterToday + selectedSize
                 progress = (100.0 * waterToday / 2000).toInt()
@@ -60,16 +117,55 @@ class WaterFragment : Fragment() {
 
                 progressBar.progress = progress
 
-
-
             }
+            val selectedSize = spinner.selectedItem.toString().take(3).toInt()
+            binding.cupsLeft.text = getString(R.string.cupsLeft, ((2000 - waterToday ) / selectedSize))
 
 
         }
 
+        var stepsProgress = 0
+        var stepsGoal = 0
+        binding.btnOk.setOnClickListener {
+            pbStepsTracker.progress = 0
+            stepsGoal = binding.txtSteps.text.toString().toInt()
+            editorProgress.apply {
+                putInt("stepsGoal", stepsGoal)
+                apply()
+            }
+            binding.tvStepsProgress.text =  getString(R.string.stepsProgress, stepsProgress, stepsGoal)
+        }
 
 
 
+        val currDate = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val currDateFormatted = currDate.format(formatter)
+        val today = sharedPrefIntake.getString("dateToday", null)
+        if(today == null){
+            editorProgress.apply{
+                putString("dateToday", currDateFormatted)
+                apply()
+            }
+        } else if(currDateFormatted != today){
+            editorProgress.apply {
+                putString("dateToday", currDateFormatted)
+                putInt("stepsProgress", stepsProgress)
+                apply()
+            }
+            editorProgress.apply {
+                putInt("stepsProgress", 0)
+                putInt("waterIntake", 0)
+                putInt("waterProgress", 0)
+                stepsProgress = sharedPrefIntake.getInt("stepsProgress", 0)
+                binding.tvStepsProgress.text =  getString(R.string.stepsProgress, stepsProgress, stepsGoal)
+                binding.tvWaterIntake.text = "0ml/2000ml"
+                progressBar.progress = 0
+                binding.tvWaterProgress.text = "(0%)"
+                apply()
+
+            }
+        }
 
 
 
