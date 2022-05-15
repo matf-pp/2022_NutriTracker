cba53cdaf2bdf9fb03bf9dd8ffb6cf0dfde56f43
Jelena Lazovic <jelena3160@gmail.com>
2022-05-15 22:35:59 +0200

    Daily Fragment and UserInfo activity changes

--- app/src/main/java/com/marko590/tabtestfinal/DailyFragment.kt
+++ app/src/main/java/com/marko590/tabtestfinal/DailyFragment.kt
@@ -1,21 +1,16 @@
 package com.marko590.tabtestfinal
 import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
-import androidx.room.Query
 import com.marko590.tabtestfinal.data.Day
 import com.marko590.tabtestfinal.data.Food
 import com.marko590.tabtestfinal.data.UserViewFood
 import com.marko590.tabtestfinal.databinding.DailyFragmentBinding
-import kotlinx.android.synthetic.main.activity_user_info.*
 import kotlinx.android.synthetic.main.daily_fragment.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -25,32 +20,17 @@ import kotlin.math.roundToInt
 class DailyFragment : Fragment() {
 
     private lateinit var mUserViewModel : UserViewFood
-    var calorieIntakeNew = 0
-    var proteinIntakeNew = 0
-    var fatIntakeNew = 0
-    var carbsIntakeNew = 0
 
+    private fun calculatePercentage(curr: Float, target: Int) : Int {
+        return (curr / (target / 100.0)).roundToInt()
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         val binding = DataBindingUtil.inflate<DailyFragmentBinding>(inflater,
             R.layout.daily_fragment,container,false)
 
+
         val sharedPrefIntake = requireContext().getSharedPreferences("UserInfoPref", Context.MODE_PRIVATE)
-        val editorIntake=sharedPrefIntake.edit()
-
-        editorIntake.apply {
-
-            calculateNutrients(sharedPrefIntake)
-
-            putInt("calorieIntake", calorieIntakeNew)
-            putInt("proteinIntake", proteinIntakeNew)
-            putInt("fatIntake", fatIntakeNew)
-            putInt("carbsIntake", carbsIntakeNew)
-            apply()
-
-        }
-
-
         val calorieIntake = sharedPrefIntake.getInt("calorieIntake", 1)
         val proteinIntake = sharedPrefIntake.getInt("proteinIntake", 1)
         val fatIntake = sharedPrefIntake.getInt("fatIntake", 1)
@@ -66,58 +46,23 @@ class DailyFragment : Fragment() {
 
 
 
-        fun calculatePercentage(curr: Float, target: Int) : Int {
-            return (curr / (target / 100.0)).roundToInt()
-        }
-
         // Setting the values of text views according to current progress
         fun text(){
             binding.tvCaloriesCounter.text = getString(R.string.kCalProgress, calorieProgress, calorieIntake)
-            if(calculatePercentage(calorieProgress, calorieIntake)<100) {
-                binding.pbCaloriesTracker.progress =
-                    calculatePercentage(calorieProgress, calorieIntake)
-            }
-            else {
-                binding.pbCaloriesTracker.progress = 100
-            }
-            if(calculatePercentage(calorieProgress, calorieIntake)<100) {
-                binding.tvCaloriesIntake.text =
-                    calculatePercentage(calorieProgress, calorieIntake).toString()
-            }
-            else{
-                binding.tvCaloriesIntake.text = "100"
-            }
+            binding.pbCaloriesTracker.progress = calculatePercentage(calorieProgress, calorieIntake)
+            binding.tvCaloriesIntake.text = calculatePercentage(calorieProgress, calorieIntake).toString()
 
             binding.tvProteinIntake.text = getString(R.string.nutrientsProgress, proteinProgress, proteinIntake,
                 calculatePercentage(proteinProgress, proteinIntake))
-
-            if(calculatePercentage(proteinProgress, proteinIntake)<100) {
-                binding.pbProteinIntake.progress =
-                    calculatePercentage(proteinProgress, proteinIntake)
-            }
-            else{
-                binding.pbProteinIntake.progress = 100
-            }
+            binding.pbProteinIntake.progress = calculatePercentage(proteinProgress, proteinIntake)
 
             binding.tvFatIntake.text = getString(R.string.nutrientsProgress, fatProgress, fatIntake,
                 calculatePercentage(fatProgress, fatIntake))
-
-            if(calculatePercentage(fatProgress, fatIntake)<100){
-                binding.pbFatIntake.progress = calculatePercentage(fatProgress, fatIntake)
-            }
-            else{
-                binding.pbFatIntake.progress = 100
-            }
+            binding.pbFatIntake.progress = calculatePercentage(fatProgress, fatIntake)
 
             binding.tvCarbsIntake.text = getString(R.string.nutrientsProgress, carbsProgress, carbsIntake,
                 calculatePercentage(carbsProgress, carbsIntake))
-
-            if(calculatePercentage(carbsProgress, carbsIntake)<100) {
-                binding.pbCarbsIntake.progress = calculatePercentage(carbsProgress, carbsIntake)
-            }
-            else{
-                binding.pbCarbsIntake.progress = 100
-            }
+            binding.pbCarbsIntake.progress = calculatePercentage(carbsProgress, carbsIntake)
         }
 
         fun get(){
@@ -144,49 +89,34 @@ class DailyFragment : Fragment() {
 
             mUserViewModel.readAllFood.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
 
-                    data=it
-                })
+                data=it
+            })
             val foodName = binding.txtFoodName.text.toString()
             for(food in data!!){
                 if(food.name == foodName){
                     get()
-
-
-                    if(calculatePercentage(calorieProgress, calorieIntake)<100) {
+                    if(calorieProgress < calorieIntake) {
                         calorieProgress += food.calorie
+                        if (calorieProgress >= calorieIntake)
+                            calorieProgress = calorieIntake.toFloat()
                     }
 
-                    if(calculatePercentage(calorieProgress, calorieIntake)>100)
-                    {
-                        pbCaloriesTracker.progress=100
-
-                    }
-
-
-                    if(calculatePercentage(proteinProgress, proteinIntake)<100) {
+                    if(proteinProgress < proteinIntake){
                         proteinProgress += food.protein
-                    }
-                    if(calculatePercentage(proteinProgress, proteinIntake)>100){
-                        pbProteinIntake.progress=100
-
+                        if (proteinProgress >= proteinIntake)
+                            proteinProgress = proteinIntake.toFloat()
                     }
 
-                    if(calculatePercentage(fatProgress, fatIntake)<100) {
+                    if(fatProgress < fatIntake){
                         fatProgress += food.fat
+                        if (fatProgress >= fatIntake)
+                            fatProgress = fatIntake.toFloat()
                     }
-                    if(calculatePercentage(fatProgress, fatIntake)>100){
-                        pbFatIntake.progress=100
-
-                    }
-
-                    if(calculatePercentage(carbsProgress, carbsIntake)<100) {
+                    if(carbsProgress < carbsIntake){
                         carbsProgress += food.carbs
+                        if (carbsProgress >= carbsIntake)
+                            carbsProgress = carbsIntake.toFloat()
                     }
-                    if(calculatePercentage(carbsProgress, carbsIntake)>100){
-                        pbCarbsIntake.progress=100
-
-                    }
-
                     editorProgress.apply {
                         putFloat("calorieProgress", calorieProgress)
                         putFloat("proteinProgress", proteinProgress)
@@ -200,8 +130,7 @@ class DailyFragment : Fragment() {
                 }
             }
         }
-
-
+        
         // Reset values every other day
         val sharedPrefStorage = requireContext().getSharedPreferences("StoragePref", Context.MODE_PRIVATE)
         val editorStorage = sharedPrefStorage.edit()
@@ -218,9 +147,7 @@ class DailyFragment : Fragment() {
             }
         } else if(currDateFormatted != today){
             editorStorage.apply {
-                putString("dateYesterday", today)
                 putString("dateToday", currDateFormatted)
-                putFloat("calorieIntake", calorieProgress)
                 get()
                 mUserViewModel.addDay(Day(0,today, calorieProgress, proteinProgress, fatProgress, carbsProgress))
                 apply()
@@ -238,64 +165,6 @@ class DailyFragment : Fragment() {
 
 
         return binding.root
-    }
-
-    fun calculateNutrients(sharedPref: SharedPreferences){
-
-        var bmr = 0.0
-
-        val height = sharedPref.getInt("usrHeight", 1)
-        val weight = sharedPref.getInt("usrWeight", 1)
-        val age = sharedPref.getInt("usrAge", 1)
-        val isWoman = sharedPref.getBoolean("isWoman", true)
-        val isMan = sharedPref.getBoolean("isMan", true)
-        val activityLevel = sharedPref.getString("activityLevel","")
-
-        // Calculating user BMR using Mifflin St Jeor Equation
-        if(isMan) {
-            bmr = 10 * weight + 6.25 * height - 5 * age + 5
-        } else if(isWoman) {
-            bmr = 10 * weight + 6.25 * height - 5 * age - 161
-        }
-
-        // CalculatinNewg nutrients intake based on activity level
-        // 4 kCal = 1g of protein and carbs
-        // 9 kCal = 1g of fat
-        val activity = resources.getStringArray(R.array.activityLevel)
-        when(activityLevel){
-            activity[0] -> { // Sedentary
-                calorieIntakeNew = (bmr * 1.2).toInt()
-                proteinIntakeNew = (calorieIntakeNew * 0.1 / 4).toInt()
-                fatIntakeNew = (calorieIntakeNew * 0.25 / 9).toInt()
-                carbsIntakeNew = (calorieIntakeNew * 0.45 / 4).toInt()
-            }
-            activity[1] -> { // Lightly active
-                calorieIntakeNew = (bmr * 1.375).toInt()
-                proteinIntakeNew = (calorieIntakeNew * 0.15 / 4).toInt()
-                fatIntakeNew = (calorieIntakeNew * 0.3 / 9).toInt()
-                carbsIntakeNew = (calorieIntakeNew * 0.5 / 4).toInt()
-            }
-            activity[2] -> { // Moderately active
-                calorieIntakeNew = (bmr * 1.55).toInt()
-                proteinIntakeNew = (calorieIntakeNew * 0.2 / 4).toInt()
-                fatIntakeNew = (calorieIntakeNew * 0.3 / 9).toInt()
-                carbsIntakeNew = (calorieIntakeNew * 0.55 / 4).toInt()
-            }
-            activity[3] -> { // Active
-                calorieIntakeNew = (bmr * 1.725).toInt()
-                proteinIntakeNew = (calorieIntakeNew * 0.25 / 4).toInt()
-                fatIntakeNew = (calorieIntakeNew * 0.3 / 9).toInt()
-                carbsIntakeNew = (calorieIntakeNew * 0.6 / 4).toInt()
-            }
-            activity[4] -> { // Very active
-                calorieIntakeNew = (bmr * 1.9).toInt()
-                proteinIntakeNew = (calorieIntakeNew * 0.3 / 4).toInt()
-                fatIntakeNew = (calorieIntakeNew * 0.3 / 9).toInt()
-                carbsIntakeNew = (calorieIntakeNew * 0.65 / 4).toInt()
-            }
-        }
-
-
     }
 
 }
--- app/src/main/java/com/marko590/tabtestfinal/UserInfoActivity.kt
+++ app/src/main/java/com/marko590/tabtestfinal/UserInfoActivity.kt
@@ -13,6 +13,113 @@ import kotlinx.android.synthetic.main.activity_user_info.*
 class UserInfoActivity : AppCompatActivity() {
 
     private lateinit var mUserViewModel : UserViewFood
+
+    var bmr = 0.0
+    var calorieIntake = 0
+    var proteinIntake = 0
+    var fatIntake = 0
+    var carbsIntake = 0
+
+    private fun addDatabase(){
+        mUserViewModel= ViewModelProvider(this)[UserViewFood::class.java]
+        mUserViewModel.addFood(Food(0, "Banana", 92f, 1.1f,0.33f ,22.84f))
+        mUserViewModel.addFood(Food(0, "Apple", 52f,0.03f, 0.17f, 13.8f))
+        mUserViewModel.addFood(Food(0, "Fries", 220f,3.4f, 5.48f, 25.55f))
+        mUserViewModel.addFood(Food(0, "Cabbage", 25f,1.28f, 0.1f,5.8f))
+        mUserViewModel.addFood(Food(0, "Onion", 40f,1.1f,0.1f,9.3f ))
+        mUserViewModel.addFood(Food(0, "Yogurt", 61f,3.5f, 3.3f, 4.7f ))
+        mUserViewModel.addFood(Food(0, "Milk", 64f, 3.3f, 3.8f, 4.7f))
+        mUserViewModel.addFood(Food(0, "Tomato", 19f,0.9f, 0.2f,3.9f))
+        mUserViewModel.addFood(Food(0, "Pork", 297f, 25.7f, 20.8f, 0f))
+        mUserViewModel.addFood(Food(0, "Chicken breast", 118f,22.5f, 2.62f,0f ))
+        mUserViewModel.addFood(Food(0, "Omelette", 167f, 11.2f, 12.1f,2.1f))
+        mUserViewModel.addFood(Food(0, "Salmon", 206f,22f,12f,0f))
+        mUserViewModel.addFood(Food(0, "Bread", 265f, 9f, 3.2f,49f ))
+        mUserViewModel.addFood(Food(0, "Donut", 452f, 4.9f, 25f, 51f ))
+        mUserViewModel.addFood(Food(0, "Cereals", 389f, 16.89f, 6.9f, 66.27f ))
+        mUserViewModel.addFood(Food(0, "Rice", 360f,6.61f,0.58f,79.34f))
+        mUserViewModel.addFood(Food(0, "Pizza", 266f, 11f, 10f,33f ))
+        mUserViewModel.addFood(Food(0, "Spaghetti", 157f, 6f, 0.9f, 31f ))
+        mUserViewModel.addFood(Food(0, "Chocolate", 545f, 4.9f, 31f, 61f ))
+        mUserViewModel.addFood(Food(0, "Ice cream", 207f, 3.5f, 11f,24f))
+        mUserViewModel.addFood(Food(0, "Coca Cola", 44f,0f, 0f, 12f))
+        mUserViewModel.addFood(Food(0, "Red wine",85f, 0.1f, 0f, 2.6f ))
+    }
+
+    private fun calculateNutrients(){
+
+        val height = itHeight.text.toString().toDouble()
+        val weight = itWeight.text.toString().toDouble()
+        val age = itAge.text.toString().toDouble()
+        val isWoman = rbWoman.isChecked
+        val isMan = rbMan.isChecked
+        val activityLevel = sActivity.selectedItem.toString()
+
+        // Calculating user BMR using Mifflin St Jeor Equation
+        if(isMan) {
+            bmr = 10.0 * weight + 6.25 * height - 5 * age + 5
+        } else if(isWoman) {
+            bmr = 10 * weight + 6.25 * height - 5 * age - 161
+        }
+
+        // Calculating nutrients intake based on activity level
+        // 4 kCal = 1g of protein and carbs
+        // 9 kCal = 1g of fat
+        val activity = resources.getStringArray(R.array.activityLevel)
+        when(activityLevel){
+            activity[0] -> { // Sedentary
+                calorieIntake = (bmr * 1.2).toInt()
+                proteinIntake = (calorieIntake * 0.1 / 4).toInt()
+                fatIntake = (calorieIntake * 0.25 / 9).toInt()
+                carbsIntake = (calorieIntake * 0.45 / 4).toInt()
+            }
+            activity[1] -> { // Lightly active
+                calorieIntake = (bmr * 1.375).toInt()
+                proteinIntake = (calorieIntake * 0.15 / 4).toInt()
+                fatIntake = (calorieIntake * 0.3 / 9).toInt()
+                carbsIntake = (calorieIntake * 0.5 / 4).toInt()
+            }
+            activity[2] -> { // Moderately active
+                calorieIntake = (bmr * 1.55).toInt()
+                proteinIntake = (calorieIntake * 0.2 / 4).toInt()
+                fatIntake = (calorieIntake * 0.3 / 9).toInt()
+                carbsIntake = (calorieIntake * 0.55 / 4).toInt()
+            }
+            activity[3] -> { // Active
+                calorieIntake = (bmr * 1.725).toInt()
+                proteinIntake = (calorieIntake * 0.25 / 4).toInt()
+                fatIntake = (calorieIntake * 0.3 / 9).toInt()
+                carbsIntake = (calorieIntake * 0.6 / 4).toInt()
+            }
+            activity[4] -> { // Very active
+                calorieIntake = (bmr * 1.9).toInt()
+                proteinIntake = (calorieIntake * 0.3 / 4).toInt()
+                fatIntake = (calorieIntake * 0.3 / 9).toInt()
+                carbsIntake = (calorieIntake * 0.65 / 4).toInt()
+            }
+        }
+    }
+
+    // Check and display error message if input is missing
+    private fun validateFields():Boolean {
+        if(itName.text.toString().isEmpty()){
+            itName.error = "Name is required"
+            return false
+        }
+        if(itHeight.text.toString().isEmpty()) {
+            itHeight.error = "Height is required"
+            return false
+        }
+        if(itWeight.text.toString().isEmpty()) {
+            itWeight.error = "Weight is required"
+            return false
+        }
+        if(itAge.text.toString().isEmpty()) {
+            itAge.error = "Age is required"
+            return false
+        }
+        return true
+    }
     override fun onResume() {
         super.onResume()
         val sharedPref = getSharedPreferences("UserInfoPref", Context.MODE_PRIVATE)
@@ -24,7 +131,7 @@ class UserInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
 
-
+        addDatabase()
 
         // Creating spinner for activity level
         val listActivity = resources.getStringArray(R.array.activityLevel)
@@ -34,88 +141,7 @@ class UserInfoActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
-        // Check and display error message if input is missing
-        fun validateFields():Boolean {
-            if(itName.text.toString().isEmpty()){
-                itName.error = "Name is required"
-                return false
-            }
-            if(itHeight.text.toString().isEmpty()) {
-                itHeight.error = "Height is required"
-                return false
-            }
-            if(itWeight.text.toString().isEmpty()) {
-                itWeight.error = "Weight is required"
-                return false
-            }
-            if(itAge.text.toString().isEmpty()) {
-                itAge.error = "Age is required"
-                return false
-            }
-            return true
-        }
-
         val sharedPref = getSharedPreferences("UserInfoPref", Context.MODE_PRIVATE)
-        var bmr = 0.0
-        var calorieIntake = 0
-        var proteinIntake = 0
-        var fatIntake = 0
-        var carbsIntake = 0
-
-        fun calculateNutrients(){
-
-            val height = itHeight.text.toString().toInt()
-            val weight = itWeight.text.toString().toInt()
-            val age = itAge.text.toString().toInt()
-            val isWoman = rbWoman.isChecked
-            val isMan = rbMan.isChecked
-            val activityLevel = sActivity.selectedItem.toString()
-
-            // Calculating user BMR using Mifflin St Jeor Equation
-            if(isMan) {
-                bmr = 10 * weight + 6.25 * height - 5 * age + 5
-            } else if(isWoman) {
-                bmr = 10 * weight + 6.25 * height - 5 * age - 161
-            }
-
-            // Calculating nutrients intake based on activity level
-            // 4 kCal = 1g of protein and carbs
-            // 9 kCal = 1g of fat
-            val activity = resources.getStringArray(R.array.activityLevel)
-            when(activityLevel){
-                activity[0] -> { // Sedentary
-                    calorieIntake = (bmr * 1.2).toInt()
-                    proteinIntake = (calorieIntake * 0.1 / 4).toInt()
-                    fatIntake = (calorieIntake * 0.25 / 9).toInt()
-                    carbsIntake = (calorieIntake * 0.45 / 4).toInt()
-                }
-                activity[1] -> { // Lightly active
-                    calorieIntake = (bmr * 1.375).toInt()
-                    proteinIntake = (calorieIntake * 0.15 / 4).toInt()
-                    fatIntake = (calorieIntake * 0.3 / 9).toInt()
-                    carbsIntake = (calorieIntake * 0.5 / 4).toInt()
-                }
-                activity[2] -> { // Moderately active
-                    calorieIntake = (bmr * 1.55).toInt()
-                    proteinIntake = (calorieIntake * 0.2 / 4).toInt()
-                    fatIntake = (calorieIntake * 0.3 / 9).toInt()
-                    carbsIntake = (calorieIntake * 0.55 / 4).toInt()
-                }
-                activity[3] -> { // Active
-                    calorieIntake = (bmr * 1.725).toInt()
-                    proteinIntake = (calorieIntake * 0.25 / 4).toInt()
-                    fatIntake = (calorieIntake * 0.3 / 9).toInt()
-                    carbsIntake = (calorieIntake * 0.6 / 4).toInt()
-                }
-                activity[4] -> { // Very active
-                    calorieIntake = (bmr * 1.9).toInt()
-                    proteinIntake = (calorieIntake * 0.3 / 4).toInt()
-                    fatIntake = (calorieIntake * 0.3 / 9).toInt()
-                    carbsIntake = (calorieIntake * 0.65 / 4).toInt()
-                }
-            }
-        }
-
         val editor = sharedPref.edit()
 
         fabNext.setOnClickListener {
@@ -141,33 +167,7 @@ class UserInfoActivity : AppCompatActivity() {
                 moveToSecondary()
             }
         }
-
-        mUserViewModel= ViewModelProvider(this)[UserViewFood::class.java]
-        mUserViewModel.addFood(Food(0, "Banana", 92f, 1.1f,0.33f ,22.84f))
-        mUserViewModel.addFood(Food(0, "Apple", 52f,0.03f, 0.17f, 13.8f))
-        mUserViewModel.addFood(Food(0, "Fries", 220f,3.4f, 5.48f, 25.55f))
-        mUserViewModel.addFood(Food(0, "Cabbage", 25f,1.28f, 0.1f,5.8f))
-        mUserViewModel.addFood(Food(0, "Onion", 40f,1.1f,0.1f,9.3f ))
-        mUserViewModel.addFood(Food(0, "Yogurt", 61f,3.5f, 3.3f, 4.7f ))
-        mUserViewModel.addFood(Food(0, "Milk", 64f, 3.3f, 3.8f, 4.7f))
-        mUserViewModel.addFood(Food(0, "Tomato", 19f,0.9f, 0.2f,3.9f))
-        mUserViewModel.addFood(Food(0, "Pork", 297f, 25.7f, 20.8f, 0f))
-        mUserViewModel.addFood(Food(0, "Chicken breast", 118f,22.5f, 2.62f,0f ))
-        mUserViewModel.addFood(Food(0, "Omelette", 167f, 11.2f, 12.1f,2.1f))
-        mUserViewModel.addFood(Food(0, "Salmon", 206f,22f,12f,0f))
-        mUserViewModel.addFood(Food(0, "Bread", 265f, 9f, 3.2f,49f ))
-        mUserViewModel.addFood(Food(0, "Donut", 452f, 4.9f, 25f, 51f ))
-        mUserViewModel.addFood(Food(0, "Cereals", 389f, 16.89f, 6.9f, 66.27f ))
-        mUserViewModel.addFood(Food(0, "Rice", 360f,6.61f,0.58f,79.34f))
-        mUserViewModel.addFood(Food(0, "Pizza", 266f, 11f, 10f,33f ))
-        mUserViewModel.addFood(Food(0, "Spaghetti", 157f, 6f, 0.9f, 31f ))
-        mUserViewModel.addFood(Food(0, "Chocolate", 545f, 4.9f, 31f, 61f ))
-        mUserViewModel.addFood(Food(0, "Ice cream", 207f, 3.5f, 11f,24f))
-        mUserViewModel.addFood(Food(0, "Coca Cola", 44f,0f, 0f, 12f))
-        mUserViewModel.addFood(Food(0, "Red wine",85f, 0.1f, 0f, 2.6f ))
-
     }
-
 
 
     private fun moveToSecondary(){
